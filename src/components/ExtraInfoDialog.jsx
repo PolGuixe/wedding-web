3efe7d263bc2be5138948e72e5d90875f18d84a5
PolGuixe <pol.guixe@gmail.com>
2018-09-12 23:56:13 +0200

    added extra info component and info

---
+++ src/components/ExtraInfoDialog.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Button from '@material-ui/core/Button'
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import withMobileDialog from '@material-ui/core/withMobileDialog'
+import { Typography } from '@material-ui/core'
+
+class ResponsiveDialog extends React.Component {
+  state = {
+    open: false,
+  }
+
+  handleClickOpen = () => {
+    this.setState({ open: true })
+  }
+
+  handleClose = () => {
+    this.setState({ open: false })
+  }
+
+  render() {
+    const { fullScreen, buttonText, title, content, actionText } = this.props
+
+    return (
+      <div>
+        <Button onClick={this.handleClickOpen} color="inherit">{buttonText}</Button>
+        <Dialog
+          fullScreen={fullScreen}
+          open={this.state.open}
+          onClose={this.handleClose}
+          aria-labelledby="responsive-dialog-title"
+        >
+          <DialogTitle id="responsive-dialog-title">{title}</DialogTitle>
+          <DialogContent>
+            {content.map(sentence => <Typography variant="body1" paragraph>{sentence}</Typography>)}
+          </DialogContent>
+          <DialogActions>
+            <Button
+              onClick={this.handleClose}
+              color="primary"
+              variant="raised"
+              autoFocus
+            >
+              {actionText}
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    )
+  }
+}
+
+ResponsiveDialog.propTypes = {
+  fullScreen: PropTypes.bool.isRequired,
+}
+
+export default withMobileDialog()(ResponsiveDialog)
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,46 @@ import Img from 'gatsby-image'
 import Countdown from 'react-countdown-now'
 
 import Logo from '../components/LP-logo'
+import ExtraInfoDialog from '../components/ExtraInfoDialog'
+
+const moreInfo = [
+  {
+    buttonText: 'Más información',
+    title: 'Prepárate...',
+    content: [
+      'Pol quería una boda sencilla..., así que Laura decidió que organizaríamos un fiestón 🎉 en bonito pueblo en el Pirineo para todos vosotros ⛰️ ',
+      'Si no te quieres perder nada, te esperamos el viernes 12 a última hora de la tarde 🍔🍷',
+      'La ceremonia será el sábado 13 al mediodía 👰🏻🤵🏻',
+      'Después de bailar toda la noche, el domingo 14 repondremos fuerzas durante el “brunch” 🍳🥐 antes de volver a casa ',
+      'En las próximas semanas daremos más detalles sobre la agenda y la logística. Si necesitas reservar desplazamientos urgentemente, ¡no dudes en contactarnos!✈️',
+    ],
+    actionText: '¡Mola!',
+  },
+  {
+    buttonText: 'Més informació',
+    title: 'Prepareu-vos...',
+    content: [
+      'En Pol volia una boda senzilla..., per tant la Laura va decidir que prepararíem una bona festa 🎉 en un bonic poble en el Pirineu per a tots vosaltres⛰️ ',
+      'Si no et vols perdre res, t’esperem divendres 12 al vespre 🍔🍷',
+      'La cerimònia serà dissabte 13 al migdia 👰🏻🤵🏻',
+      'Després de ballar tota la nit, el diumenge 14 agafarem forces durant el “brunch” 🍳🥐 avans de tornar a casa',
+      'Durant les setmanes vinents us donarem més informació de l’agenda i la logística. Si has de reservar el viatge urgentment, no dubtis en contactar-nos! ✈️',
+    ],
+    actionText: 'Guai!',
+  },
+  {
+    buttonText: 'More information',
+    title: 'Get ready...',
+    content: [
+      'Pol wanted a simple wedding..., so Laura decided to organize a big party 🎉 in a beautiful village in the Pyrenees for you all ⛰️ ',
+      'If you don’t want to miss anything, we hope to see you there Friday 12th in the evening  🍔🍷',
+      'The ceremony will take place on Saturday 13th at midday 👰🏻🤵🏻',
+      'After dancing all night, Sunday 14th in the morning we’ll enjoy brunch 🍳🥐 before heading back home',
+      'In the coming weeks we’ll share more details about the agenda and the logistics. If you need to make your travel arrangements urgently, don’t hesitate to contact us! ✈️',
+    ],
+    actionText: 'Cool!',
+  },
+]
 
 const styles = theme => ({
   container: {
@@ -67,9 +107,6 @@ const styles = theme => ({
     borderWidth: '3px',
     fontSize: '18px',
   },
-  hastag: {
-    fontFamily: ['Bangers'],
-  },
 })
 
 const IndexPage = ({ classes, data }) => (
@@ -77,17 +114,7 @@ const IndexPage = ({ classes, data }) => (
     <Grid item xs={12} sm={6} className={classes.picture}>
       <div className={classes.section}>
         <Typography variant="display2" color="primary">
-          Laura
-        </Typography>
-        <Typography
-          variant="display3"
-          className={classes.plusText}
-          color="primary"
-        >
-          +
-        </Typography>
-        <Typography variant="display2" color="primary">
-          Pol
+          Laura + Pol
         </Typography>
         <Img
           outerWrapperClassName={classes.imageOuterWrapper}
@@ -99,9 +126,9 @@ const IndexPage = ({ classes, data }) => (
           - Save the date, we're launching 🚀 a new adventure -
         </Typography>
         <Typography variant="headline">
-        <Countdown date={new Date(2019,3,13,11)}>
-          We are married!
-        </Countdown>
+          <Countdown date={new Date(2019, 3, 13, 11)}>
+            We are married!
+          </Countdown>
         </Typography>
       </div>
     </Grid>
@@ -109,11 +136,11 @@ const IndexPage = ({ classes, data }) => (
       <div className={classes.section}>
         <Logo className={classes.svgLogo} />
         <Typography variant="subheading" color="inherit">
-          ¿Cuando? Quan? When?
+          ¿Cuándo? Quan? When?
         </Typography>
         <div className={classes.divider} />
         <Typography variant="title" color="inherit">
-          13 avr. 2019 a 13:00
+          12-13-14 / 04 / 2019
         </Typography>
         <div className={classes.spacer} />
         <Typography variant="subheading" color="inherit">
@@ -121,21 +148,20 @@ const IndexPage = ({ classes, data }) => (
         </Typography>
         <div className={classes.divider} />
         <Typography variant="title" color="inherit">
-          Liguerre Resort
+          Ligüerre Resort
         </Typography>
         <Typography variant="title" color="inherit">
-          Calle Santa Orosia Ag Alberuela, 2, 22393
+          Huesca - Spain
         </Typography>
-        <Typography variant="title" color="inherit">
-          Abizanda, Huesca, Spain
-        </Typography>
+        <div className={classes.spacer} />
+        {moreInfo.map(info => <ExtraInfoDialog {...info} />)}
         <div className={classes.spacer} />
         <Button variant="outlined" color="inherit" className={classes.button}>
-          ¡Voy! Vaig! Coming! On y va!
+          ¡Voy! Vaig! Coming!
         </Button>
         <div className={classes.spacer} />
-        <Typography variant="title" color="inherit" className={classes.hastag}>
-          #laura+pol
+        <Typography variant="display1" color="inherit">
+          #laura❤️pol
         </Typography>
       </div>
     </Grid>
@@ -148,7 +174,7 @@ export const query = graphql`
   query LandingPageQuery {
     landingImage: imageSharp(id: { regex: "/LP-landing-photo/" }) {
       sizes(maxWidth: 800) {
-        ...GatsbyImageSharpSizes_tracedSVG
+        ...GatsbyImageSharpSizes_withWebp_tracedSVG
       }
     }
   }

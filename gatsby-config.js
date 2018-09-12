9be6bc6f5d8548bae2724eebad07101b2d97716f
PolGuixe <pol.guixe@gmail.com>
2018-09-13 00:49:21 +0200

    added favicons and manifest and offline plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,5 +13,36 @@ module.exports = {
     },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/images/icon.png",
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false
+        }
+      }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Laura + Pol',
+        short_name: 'Laura + Pol',
+        start_url: '/',
+        background_color: '#f7f0eb',
+        theme_color: '#213443',
+        display: 'minimal-ui',
+        icon: 'src/images/icon.png', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-offline',
   ],
 }

56f60447c8acc96c5036cb14a5ac5d8a85505da0
PolGuixe <pol.guixe@gmail.com>
2019-01-13 20:10:38 +0100

    added new page for laura's coworkers

---
+++ src/pages/vamos-de-boda.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import Layout from 'material-ui-layout'
+import withStyles from '@material-ui/core/styles/withStyles'
+
+import Agenda from '../components/Agenda/agendaES'
+import Accommodation from '../components/Accommodation/accomodationNoPriceES'
+import Travel from '../components/Travel/travelES'
+import Packing from '../components/Packing/packingES'
+import RSVP from '../components/RSVP/rsvpES'
+import Contact from '../components/Contact/contactES'
+
+const styles = theme => ({
+  moreInfoPageChildren: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '100%',
+  },
+})
+
+class VamosDeFiesta extends Component {
+  render() {
+    const { classes } = this.props
+    return (
+      <Layout>
+        <div className={classes.moreInfoPageChildren}>
+          <RSVP />
+          <Agenda />
+          <Accommodation />
+          <Travel />
+          <Packing />
+          <Contact  dark/>
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export default withStyles(styles)(VamosDeFiesta)
